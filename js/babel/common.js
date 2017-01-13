c8c600b471158615f8a936823cf93a670572322e
Nirrvaa <mrbi220099@gmail.com>
2017-01-13 15:03:25 +0200

    ДОбавлен полифилл для ИЕ. Анимация работает корректно (вроде)))

--- js/babel/common.js
+++ js/babel/common.js
@@ -1,4 +1,18 @@
 let lockButton = document.getElementById('fixed');
+let description = document.querySelector('.description');
+let answers = document.querySelector('.answers');
+let insideNavigation = document.querySelector('.inside-navigation');
+description.addEventListener('click', function(event) {
+	let target = event.target;
+	changeContent.call(this,target,fixed);
+});
+
+answers.addEventListener('click', function(event) {
+	let target = event.target;
+	changeContent.call(this,target,fixed,insideNavigation);
+
+});
+
 let changingElementsOpacity = document.querySelectorAll('[data-opacity]');
 let changingElementsVisibility = document.querySelectorAll('[data-visibility]');
 let changingElementPosition = document.querySelectorAll('[data-position]');
@@ -20,6 +34,8 @@ lockButton.addEventListener('click', function(event) {
 });
 
 
+
+
 function changeElements(enteringArray,value,clas,attribute,start,finish) {
 	let elements = [...enteringArray];
 	for (let elem of elements) {
@@ -27,11 +43,43 @@ function changeElements(enteringArray,value,clas,attribute,start,finish) {
 			elem.classList.add(clas);
 		} else if (value == finish) {
 			elem.classList.remove(clas);
-			elem.setAttribute(attribute, 0);
+			elem.setAttribute(attribute, value);
 			continue;
 		}
 		elem.setAttribute(attribute, +value+1); 
 	}
+}
+
+function changeContent(target,menu,nav) {
+	let array = this.children;
+	let fixed = menu;
+	let check = fixed.getAttribute('data-value');
+	if (target == array[0] || target == array[2]) {
+		let value = this.getAttribute('data-counter');
+		if (value == 2) {
+			value=0;
+		}
+		value++
+		if (!nav) {
+			if (!+check) {
+				changeElements(array, value,'visible','data-opacity', 1,2);
+				changeElements(array, value,'change-view','data-position', 1,2);
+			} else {
+				changeElements(array, value,'change-view','data-position', 1,2);
+			}
+		} else {
+			
+			if (!+check) {
+				changeElements(array, value,'visible','data-opacity', 1,2);
+				changeElements(array, value,'change-view','data-position', 1,2);
+				changeElements([nav], value,'change-view','data-position', 1,2);
+			} else {
+				changeElements(array, value,'change-view','data-position', 1,2);
+				changeElements([nav], value,'change-view','data-position', 1,2);
+			}
+		}
+		this.setAttribute('data-counter', value);
+		}
 }
 
 
